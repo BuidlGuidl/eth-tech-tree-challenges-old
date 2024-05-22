df6db279d6b3c6c0fce0894ac80a8a0b451f04f8
escottalexander@gmail.com <escottalexander@gmail.com>
2024-05-22 16:24:43 -0400

    add console2 import

--- packages/foundry/contracts/WrappedETH.sol
+++ packages/foundry/contracts/WrappedETH.sol
@@ -1,5 +1,6 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
+import { console2 } from "forge-std/console2.sol";
 
 contract WrappedETH {
     // ERC20 Standard methods for token metadata
