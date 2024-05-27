33dab9f33b2a4d48bf9fe01611ea0db414227c12
Sam Wellander <samwellander@gmail.com>
2024-05-27 15:44:24 -0700

    rename contract

--- packages/foundry/contracts/SocialRecoveryWallet.sol
+++ packages/foundry/contracts/SocialRecoveryWallet.sol
@@ -2,6 +2,6 @@
 pragma solidity >=0.8.0 <0.9.0;
 import { console2 } from "forge-std/console2.sol";
 
-contract Challenge {
+contract SocialRecoveryWallet {
     // Add the challenges contract here
 }
