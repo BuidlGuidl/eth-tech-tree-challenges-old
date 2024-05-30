225b8185517fd633a7e68f559ac419ec23c52071
Sam Wellander <samwellander@gmail.com>
2024-05-30 11:19:43 -0700

    update Deploy script contract
    to deploy social recovery wallet

--- packages/foundry/script/Deploy.s.sol
+++ packages/foundry/script/Deploy.s.sol
@@ -1,11 +1,17 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "../contracts/Challenge.sol";
+import "../contracts/SocialRecoveryWallet.sol";
 import "./DeployHelpers.s.sol";
 
 contract DeployScript is ScaffoldETHDeploy {
     error InvalidPrivateKey(string);
+
+    address guardian0 = 0x0b3aA6f7e5be55E7012A8677779B41487B424F70;
+    address guardian1 = 0x09F1E981Ac9c32D3E88819b0cE091Dc27f9cf857;
+    address guardian2 = 0x62bA14f9BBAe5aF1fE4b4cA4339d9ee332750E3F;
+
+    address[] chosenGuardianList = [guardian0, guardian1, guardian2];
 
     function run() external {
         uint256 deployerPrivateKey = setupLocalhostEnv();
@@ -15,11 +21,12 @@ contract DeployScript is ScaffoldETHDeploy {
             );
         }
         vm.startBroadcast(deployerPrivateKey);
-        Challenge challenge = new Challenge();
+
+        SocialRecoveryWallet socialRecoveryWallet = new SocialRecoveryWallet(chosenGuardianList, 2);
         console.logString(
             string.concat(
-                "Challenge deployed at: ",
-                vm.toString(address(challenge))
+                "SocialRecoveryWallet deployed at: ",
+                vm.toString(address(socialRecoveryWallet))
             )
         );
         vm.stopBroadcast();
