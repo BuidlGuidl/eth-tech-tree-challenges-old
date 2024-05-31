0b6c762b6db9a36b70883c4fb298392c4f6f77c0
steve0xp <stevep0xp@gmail.com>
2024-05-31 16:28:55 -0500

    Update RebasingERC20.sol & tests

--- packages/foundry/contracts/RebasingERC20.sol
+++ packages/foundry/contracts/RebasingERC20.sol
@@ -7,34 +7,87 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 /**
  * @title RebasingERC20 Challenge Contract
  * @author BUIDL GUIDL
- * @notice This challenge contract is meant to be an example rebasing token.
+ * @notice This challenge contract is meant to be an ERC-20 token with a rebasing mechanism to adjust total supply.
  * @dev The natspec is meant to be paired with the README.md to help guide you through this challenge! Goodluck!
  * @dev This smart contract is PURELY EDUCATIONAL, and is not to be used in production code. It is up to the user's discretion to make their own production code, run tests, have audits, etc.
  */
 contract RebasingERC20 is ERC20, Ownable {
-    uint256 private _totalSupply;
+
+    /// Errors
+    /**
+     * @notice Sender requires enough ETH for the transaction.
+     */
+    error RebasingERC20__InsufficientBalance(address _sender, uint256 _amount);
+
+    /**
+     * @notice Cannot use bad epoch values in rebase.
+     */
+    error RebasingERC20__BadEpoch(uint256 _epoch);
+
+    /**
+     * @notice Cannot use bad _supplyDelta values in rebase.
+     */
+    error RebasingERC20__InvalidSupplyDelta(uint256 _supplyDelta);
+
+    /// State Vars
+    uint256 public _totalSupply;
     mapping(address => uint256) private _balances;
-    uint256 private _scalingFactor;
+    uint256 public _scalingFactor;
+    uint256 public _initialSupply;
+    /// Events
 
-    event LogRebase(uint256 indexed epoch, uint256 totalSupply);
+    /**
+     * @dev Emitted when a rebase occurs.
+     * @param epoch The epoch number of the rebase event.
+     * @param totalSupply The new total supply of the token after the rebase.
+     */
+    event Rebase(uint256 indexed epoch, uint256 totalSupply);
 
+    /**
+     * @notice Sets Total Supply and Scaling Factor.
+     * @dev Constructor that gives msg.sender all of the existing tokens.
+     */
     constructor() ERC20("RebasingToken", "RBT") {
         _totalSupply = 1000000 * 10 ** decimals();
         _scalingFactor = 10 ** 18; // Initial scaling factor (1.0)
         _mint(msg.sender, _totalSupply);
+        _initialSupply = _totalSupply;
     }
 
+    /**
+     * @notice Overridden `balanceOf()` function from ERC20.sol because rebasing token rebases using scaling factor in this contract.
+     * @param account The address querying for its balance.
+     * @return Number of tokens account has at this time.
+     */
     function balanceOf(address account) public view override returns (uint256) {
         return _balances[account] * _scalingFactor / (10 ** 18);
     }
 
+    /**
+     * @notice Returns the total supply of the token.
+     * @return The total supply of the token.
+     */
     function totalSupply() public view override returns (uint256) {
         return _totalSupply;
     }
 
+    /**
+     * @dev Adjusts the total supply of the token.
+     * @param epoch The epoch number of the rebase event.
+     * @param supplyDelta The amount to increase or decrease the total supply by.
+     * @return The new total supply of the token.
+     * Requirements:
+     * - Revert with`RebasingERC20__BadEpoch` if improper epoch provided
+     * - Simply return current _totalSupply if supplyDelta == 0
+     * - Increment _totalSupply based on supplyDelta being > or < 0
+     * - Calculate new _scalingFactor based on _totalSupply and _initialSupply()
+     * - emit `Rebase` event
+     * - return _totalSupply
+     */
     function rebase(uint256 epoch, int256 supplyDelta) external onlyOwner returns (uint256) {
+        if(epoch == 0) revert RebasingERC20__BadEpoch(epoch);
         if (supplyDelta == 0) {
-            emit LogRebase(epoch, _totalSupply);
+            emit Rebase(epoch, _totalSupply);
             return _totalSupply;
         }
 
@@ -44,16 +97,23 @@ contract RebasingERC20 is ERC20, Ownable {
             _totalSupply += uint256(supplyDelta);
         }
 
-        _scalingFactor = (10 ** 18) * _totalSupply / _initialTotalSupply();
+        _scalingFactor = (10 ** 18) * _totalSupply / _initialSupply;
 
-        emit LogRebase(epoch, _totalSupply);
+        emit Rebase(epoch, _totalSupply);
         return _totalSupply;
     }
 
-    function _initialTotalSupply() internal view returns (uint256) {
-        return 1000000 * 10 ** decimals();
-    }
-
+    /**
+     * @dev Hook that is called before any transfer of tokens. This includes minting and burning.
+     * @param from The address from which tokens are transferred.
+     * @param to The address to which tokens are transferred.
+     * @param amount The amount of tokens to be transferred.
+     * Requirements
+     * - Write conditional statement for minting, aka when from address is address(0)
+     * - Write conditional statement for burning, aka when to address is address(0)
+     * - Finally handle if the sequence is just a transference of tokens.
+     * - For all of the above, make sure to do the proper increase or decrease of token balances whilst taking into account the _scalingFactor.
+     */
     function _beforeTokenTransfer(address from, address to, uint256 amount) internal override {
         if (from == address(0)) {
             // Minting tokens
@@ -68,17 +128,33 @@ contract RebasingERC20 is ERC20, Ownable {
         }
     }
 
+    /**
+     * @dev Transfers `amount` tokens from the caller's account to `recipient`.
+     * @param recipient The address to transfer tokens to.
+     * @param amount The amount of tokens to transfer.
+     * @return A boolean indicating whether the operation succeeded.
+     * Requirements:
+     * - Use the `_beforeTokenTransfer()` hook to handle the various ways that transfer is being called.
+     * - Emit the Transfer event as per ERC20 standard.
+     */
     function transfer(address recipient, uint256 amount) public override returns (bool) {
         _beforeTokenTransfer(_msgSender(), recipient, amount);
         emit Transfer(_msgSender(), recipient, amount);
         return true;
     }
 
-    function approve(address spender, uint256 amount) public override returns (bool) {
-        _approve(_msgSender(), spender, amount);
-        return true;
-    }
-
+    /**
+     * @dev Carries out transferFrom() but with internal accounting in order to take into account _scalingFactor.
+     * @param sender The address to transfer tokens from.
+     * @param recipient The address to transfer tokens to.
+     * @param amount The amount of tokens to transfer.
+     * @return A boolean indicating whether the operation succeeded.
+     * Requirements:
+     * - Use the `_beforeTokenTransfer()` hook to handle the various ways that transfer is being called.
+     * - Approve the sender the appropriate amount.
+     * - Emit the Transfer event as per ERC20 standard.
+     * - Return whether or not the tx was successful.
+     */
     function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
         _beforeTokenTransfer(sender, recipient, amount);
         _approve(sender, _msgSender(), allowance(sender, _msgSender()) - amount);
--- packages/foundry/test/RebasingERC20.t.sol
+++ packages/foundry/test/RebasingERC20.t.sol
@@ -15,25 +15,38 @@ contract RebasingERC20Test is Test {
     address luffy;
     // World's Greatest Swordsman
     address zoro;
+    uint256 luffyBalance1;
+    uint256 zoroBalance1;
+    uint256 initialBalance;
+
+    /**
+     * @dev Emitted when a rebase occurs.
+     * @param epoch The epoch number of the rebase event.
+     * @param totalSupply The new total supply of the token after the rebase.
+     */
+    event Rebase(uint256 indexed epoch, uint256 totalSupply);
 
     function setUp() public {
         luffy = address(this);
         zoro = address(0x123);
         token = new RebasingERC20();
         token.transfer(zoro, 1000 * 10 ** token.decimals());
+        luffyBalance1 = token.balanceOf(luffy);
+        zoroBalance1 = token.balanceOf(zoro);
+        initialBalance = 1000000 * 10 ** token.decimals();
     }
 
     function testInitialSupply() public {
-        assertEq(token.totalSupply(), 1000000 * 10 ** token.decimals());
+        assertEq(token.totalSupply(), initialBalance);
         assertEq(token.balanceOf(luffy), 999000 * 10 ** token.decimals());
         assertEq(token.balanceOf(zoro), 1000 * 10 ** token.decimals());
     }
 
-    function testTransfer() public {
-        uint256 transferAmount = 500 * 10 ** token.decimals();
+    function testTransfer(uint256 transferAmount) public {
+        transferAmount = bound(transferAmount, 1e18, 100e18);
         token.transfer(zoro, transferAmount);
-        assertEq(token.balanceOf(luffy), 998500 * 10 ** token.decimals());
-        assertEq(token.balanceOf(zoro), 1500 * 10 ** token.decimals());
+        assertEq(token.balanceOf(luffy), luffyBalance1 - transferAmount);
+        assertEq(token.balanceOf(zoro), zoroBalance1 + transferAmount);
     }
 
     function testApproveAndTransferFrom() public {
@@ -45,34 +58,72 @@ contract RebasingERC20Test is Test {
         // Simulate `transferFrom` by the zoro
         vm.prank(zoro);
         token.transferFrom(luffy, zoro, transferAmount);
-        assertEq(token.balanceOf(luffy), 998700 * 10 ** token.decimals());
-        assertEq(token.balanceOf(zoro), 1300 * 10 ** token.decimals());
+        assertEq(token.balanceOf(luffy), luffyBalance1 - transferAmount);
+        assertEq(token.balanceOf(zoro), zoroBalance1 + transferAmount);
         assertEq(token.allowance(luffy, zoro), approveAmount - transferAmount);
     }
 
     function testRebasePositive() public {
         uint256 epoch = 1;
-        int256 supplyDelta = int256(100000 * 10 ** token.decimals());
+        int256 supplyDelta = int256(initialBalance);
         uint256 oldTotalSupply = token.totalSupply();
-
+        uint256 expectedTotalSupply = oldTotalSupply + initialBalance;
+        
+        vm.expectEmit(true, false, false, true);
+        emit Rebase(epoch, expectedTotalSupply);
         token.rebase(epoch, supplyDelta);
         uint256 newTotalSupply = token.totalSupply();
 
         assertEq(newTotalSupply, oldTotalSupply + uint256(supplyDelta));
-        assertEq(token.balanceOf(luffy), 999000 * 10 ** token.decimals() * (newTotalSupply) / oldTotalSupply);
-        assertEq(token.balanceOf(zoro), (1000 * 10 ** token.decimals() * newTotalSupply) / oldTotalSupply);
+        assertEq(token.balanceOf(luffy), luffyBalance1* (newTotalSupply) / oldTotalSupply);
+        assertEq(token.balanceOf(zoro), (zoroBalance1 * newTotalSupply) / oldTotalSupply);
     }
 
     function testRebaseNegative() public {
         uint256 epoch = 1;
-        int256 supplyDelta = -int256(100000 * 10 ** token.decimals());
+        int256 supplyDelta = -int256(initialBalance);
         uint256 oldTotalSupply = token.totalSupply();
-
+        uint256 expectedTotalSupply = oldTotalSupply - initialBalance;
+        
+        vm.expectEmit(true, false, false, true);
+        emit Rebase(epoch, expectedTotalSupply);
         token.rebase(epoch, supplyDelta);
         uint256 newTotalSupply = token.totalSupply();
 
         assertEq(newTotalSupply, oldTotalSupply - uint256(-supplyDelta));
-        assertEq(token.balanceOf(luffy), (999000  * 10 ** token.decimals() * newTotalSupply) / oldTotalSupply);
-        assertEq(token.balanceOf(zoro), (1000  * 10 ** token.decimals() * newTotalSupply) / oldTotalSupply);
+        assertEq(token.balanceOf(luffy), (luffyBalance1 * newTotalSupply) / oldTotalSupply);
+        assertEq(token.balanceOf(zoro), (zoroBalance1 * newTotalSupply) / oldTotalSupply);
+    }
+
+    // Not Happy Path Tests
+
+    function testFailTransferInsufficientBalance() public {
+        // User tries to transfer more tokens than they have
+        vm.prank(zoro);
+        vm.expectRevert(bytes(abi.encodeWithSelector(RebasingERC20.RebasingERC20__InsufficientBalance.selector, zoro, 2000 * 10 ** token.decimals())));
+        token.transfer(luffy, 2000 * 10 ** token.decimals());
+    }
+
+    function testRebaseNotOwner() public {
+        // Non-owner tries to rebase the contract
+        vm.prank(zoro);
+        vm.expectRevert("Ownable: caller is not the owner");
+        token.rebase(1, 1000);
+    }
+
+    function testFailRebaseBadSupplyDelta() public {
+        // Owner tries to rebase the contract with a bad value for supplyDelta
+        vm.startPrank(luffy);
+        vm.expectRevert(bytes(abi.encodeWithSelector(RebasingERC20.RebasingERC20__InvalidSupplyDelta.selector, type(int256).min)));
+        token.rebase(1, type(int256).min);
+        vm.stopPrank();
+    }
+
+    function testFailRebaseBadEpoch() public {
+        // Owner tries to rebase the contract with a bad value for epoch
+        vm.startPrank(luffy);
+        vm.expectRevert(bytes(abi.encodeWithSelector(RebasingERC20.RebasingERC20__InvalidSupplyDelta.selector, 0)));
+        token.rebase(0, 1000);
+        vm.stopPrank();
     }
 }
