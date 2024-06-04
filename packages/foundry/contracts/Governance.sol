1611e20f2be1918786ae3429198efd929af89c7e
blahkheart <dannithomx@gmail.com>
2024-06-04 23:49:17 +0100

    Refactored code layout, added implementation requirements, designated code sections with block comments

--- packages/foundry/contracts/Governance.sol
+++ packages/foundry/contracts/Governance.sol
@@ -1,9 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
-import { console2 } from "forge-std/console2.sol";
 
+/*//////////////////////////////////////////////////////////////
+                               IMPORTS
+//////////////////////////////////////////////////////////////*/
+import { console2 } from "forge-std/console2.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+
+/*//////////////////////////////////////////////////////////////
+                                ERRORS
+//////////////////////////////////////////////////////////////*/
+error UnAuthorized_MembersOnly();
+error InvalidChoice();
+error DuplicateVoting();
+error VotingPeriodOver();
+error VotingInProgress();
+
+/*//////////////////////////////////////////////////////////////
+                        TYPE DECLARATIONS
+//////////////////////////////////////////////////////////////*/
 enum Choice {
     NAY,
     YEA,
@@ -16,13 +32,18 @@ struct Proposal {
     uint256 votingDeadline;
     address creator;
 }
+
+/*//////////////////////////////////////////////////////////////
+                               CONTRACT
+//////////////////////////////////////////////////////////////*/
 contract Governance {
+    /*//////////////////////////////////////////////////////////////
+                           STATE VARIABLES
+    //////////////////////////////////////////////////////////////*/
     // Token contract interface
     IERC20 public voteToken;
-
     uint256 public votingPeriod;
     uint256 private nonce;
-
     // Tracks whether an address has voted
     mapping(bytes32 => mapping(address => bool)) public hasVoted;
     // Tracks what choice an address has voted on the proposal
@@ -36,20 +57,30 @@ contract Governance {
     // Total votes to abstain
     mapping(bytes32 => uint256) public votesAbstain;
 
+    /*//////////////////////////////////////////////////////////////
+                                EVENTS
+    //////////////////////////////////////////////////////////////*/
     // Event emitted when a vote is cast
     event VoteCasted(bytes32 proposalId, address indexed voter, Choice vote, uint256 weight);
-
     // Event emitted when a proposal is created
     event ProposalCreated(bytes32 proposalId, string title, uint256 votingDeadline, address creator);
-    
+
+    /*//////////////////////////////////////////////////////////////
+                              MODIFIERS
+    //////////////////////////////////////////////////////////////*/
     /**
      * @notice Modifier to restrict access to only the token holders
      */
     modifier onlyMembers() {     
-        require(voteToken.balanceOf(msg.sender) > 0, "MembersOnly::You have no tokens");
+        if(voteToken.balanceOf(msg.sender) = 0) {
+            revert UnAuthorized_MembersOnly();
+        }
         _;
     }
 
+    /*//////////////////////////////////////////////////////////////
+                            CONSTRUCTOR
+    //////////////////////////////////////////////////////////////*/
     /**
     * @notice Constructor to initialize the governance contract
     * @param _tokenAddress The address of the ERC20 token contract
@@ -61,11 +92,21 @@ contract Governance {
         nonce = 0;
     }
 
+    /*//////////////////////////////////////////////////////////////
+                          EXTERNAL FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
     /**
      * @notice Function to create a new proposal
      * @dev Only members (token holders) can create proposals
      * @param _title The title of the proposal
      * @return proposalId The unique ID of the newly created proposal
+     * Requirements:
+     * - Only callable by members.
+     * - Creates a new Proposal
+     * - Adds new proposal to `proposals`
+     * Emits a `ProposalCreated` event.
+     */
      */
     function propose(string memory _title) external onlyMembers returns(bytes32) {
         // Generate a unique ID for the proposal using keccak256 hash
@@ -92,11 +133,27 @@ contract Governance {
      * @dev Only members (token holders) can vote
      * @param _proposalId The ID of the proposal to vote on
      * @param _support The choice of the voter (YEA, NAY, ABSTAIN)
+     * Requirements:
+     * - Only callable by members.
+     * - should revert with `InvalidChoice` if `uint(_support)` is not 0, 1, or 2.
+     * - should revert with `VotingPeriodOver` if current timestamp is over proposal's voting deadline.
+     * - should revert with `DuplicateVoting` if caller already voted for the specified proposal
+     * - Add voter weight (caller token balance) to the appropriate choice for the proposal i.e votesFor, votesAgainst, votesAbstain
+     * - Mark the voter as having voted
+     * - Record voter choice
+     * Emits a `VoteCasted` event.
      */
-    function vote(bytes32 _proposalId, Choice _support) public onlyMembers {
-        require(uint(_support) <= uint(Choice.ABSTAIN), "Invalid choice");
-        require(block.timestamp < proposals[_proposalId].votingDeadline, "Voting has ended");
-        require(!hasVoted[_proposalId][msg.sender], "You have already voted");
+     */
+    function vote(bytes32 _proposalId, Choice _support) external onlyMembers {
+        if(uint(_support) > uint(Choice.ABSTAIN)){
+            revert InvalidChoice();
+        }
+        if(block.timestamp > proposals[_proposalId].votingDeadline){
+            revert VotingPeriodOver();
+        }
+        if(hasVoted[_proposalId][msg.sender] == true){
+            revert DuplicateVoting();
+        }
         uint256 voterWeight = voteToken.balanceOf(msg.sender);
         // Update vote counts based on the support choice
         if (_support == Choice.YEA) {
@@ -113,15 +170,6 @@ contract Governance {
     }
 
     /**
-     * @notice Function to get a proposal by its ID
-     * @param _proposalId The ID of the proposal to retrieve
-     * @return The proposal struct containing its details
-     */
-    function getProposal(bytes32 _proposalId) external view returns (Proposal memory) {
-        return proposals[_proposalId];
-    }
-
-    /**
      * @notice Function to get the vote of a specific address on a specific proposal
      * @param _proposalId The ID of the proposal
      * @param _voter The address of the voter
@@ -130,23 +178,18 @@ contract Governance {
     function getVote(bytes32 _proposalId, address _voter) external view returns (Choice) {
         return choice[_proposalId][_voter];
     }
-     
-    /**
-     * @notice Function to get the voting deadline of a proposal
-     * @param _proposalId The ID of the proposal
-     * @return The voting deadline of the proposal
-     */
-    function getVotingDeadline(bytes32 _proposalId) public view returns (uint256) {
-        return proposals[_proposalId].votingDeadline;
-    }
 
     /**
      * @notice Function to get the result of a proposal vote
      * @param _proposalId The ID of the proposal
      * @return True if the votes in favor are greater than the votes against, otherwise false
+     * * Requirements:
+     * - should revert with `VotingInProgress` if current timestamp is less than or equal to proposal's voting deadline
      */
-    function getResult(bytes32 _proposalId) public view returns (bool) {
-        require(block.timestamp >= proposals[_proposalId].votingDeadline, "Voting is still ongoing");
+    function getResult(bytes32 _proposalId) external view returns (bool) {
+        if(block.timestamp <= proposals[_proposalId].votingDeadline){
+            revert VotingInProgress();
+        }
         return votesFor[_proposalId] > votesAgainst[_proposalId];
     }
 }
