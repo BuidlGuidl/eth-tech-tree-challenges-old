32847c93bc2534247f5ec2d6458a8280116e1036
Sam Wellander <samwellander@gmail.com>
2024-06-17 14:57:31 -0700

    add custom errors to Voting.sol contract

--- packages/foundry/contracts/Voting.sol
+++ packages/foundry/contracts/Voting.sol
@@ -8,6 +8,20 @@ import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract Voting {
 
     ///////////////////
+    // Errors
+    ///////////////////
+    // @dev The call does not come from the token contract
+    error Voting__NotTokenContract();
+    // @dev The call is made after the voting period has ended
+    error Voting__VotingHasEnded();
+    // @dev The call is made before the voting period has ended
+    error Voting__VotingHasNotEnded();
+    // @dev The caller has already voted
+    error Voting__AlreadyVoted();
+    // @dev The caller doesn't have enough tokens to vote
+    error Voting__NotEnoughTokens();
+
+    ///////////////////
     // State Variables
     ///////////////////
     // @dev Token contract interface
@@ -45,7 +59,9 @@ contract Voting {
      * - The caller must be the token contract
      */
     modifier onlyTokenContract() {
-        require(msg.sender == address(token), "Only token contract can call this function");
+        if (msg.sender != address(token)) {
+            revert Voting__NotTokenContract();
+        }
         _;
     }
 
@@ -74,6 +90,7 @@ contract Voting {
      * - Remove the voter's weight from voted option
      * - Resets the hasVoted flags for the voter
      * - Emits a `VotesRemoved` event
+     * - Emits a Voting__NotTokenContract error if called by anyone other than the token contract
      */
     function removeVotes(address voter) external onlyTokenContract {
         if(!hasVoted[voter]) {
@@ -100,13 +117,22 @@ contract Voting {
      * - Marks the user as having voted.
      * - Marks the user's support status.
      * - Emits a `VoteCasted` event.
+     * - Emits a `Voting__VotingHasEnded` error if voting period has ended.
+     * - Emits a `Voting__AlreadyVoted` error if caller has already voted.
+     * - Emits a `Voting__NotEnoughTokens` error if caller doesn't have a balance.
      */
     function vote(bool support) public {
-        require(block.timestamp < votingDeadline, "Voting has ended");
-        require(!hasVoted[msg.sender], "You have already voted");
+        if (block.timestamp >= votingDeadline) {
+            revert Voting__VotingHasEnded();
+        }
+        if (hasVoted[msg.sender]) {
+            revert Voting__AlreadyVoted();
+        }
 
         uint256 voterWeight = token.balanceOf(msg.sender);
-        require(voterWeight > 0, "You have no tokens to vote with");
+        if (voterWeight == 0) {
+            revert Voting__NotEnoughTokens();
+        }
 
         if (support) {
             votesFor += voterWeight;
@@ -122,11 +148,13 @@ contract Voting {
      * @dev Function to get the result of the vote
      * @return The result should be true if the majority of votes are in favor, otherwise return false
      * Requirements:
-     * - Ensure the voting period has ended
+     * - Emit a Voting__VotingHasNotEnded error if the voting period has not ended
      * - Determine the result based on the majority vote
      */
     function getResult() public view returns (bool) {
-        require(block.timestamp >= votingDeadline, "Voting is still ongoing");
+        if (block.timestamp < votingDeadline) {
+            revert Voting__VotingHasNotEnded();
+        }
         return votesFor > votesAgainst;
     }
 }
