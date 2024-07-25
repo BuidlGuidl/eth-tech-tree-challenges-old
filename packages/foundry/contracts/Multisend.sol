a998a3ebe6c5ee23636c8a8f2ed176d2127fc5c2
escottalexander@gmail.com <escottalexander@gmail.com>
2024-07-25 14:26:00 -0400

    update solc version, readme

--- packages/foundry/contracts/Multisend.sol
+++ packages/foundry/contracts/Multisend.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0 <0.9.0;
+pragma solidity 0.8.26;
 import { console2 } from "forge-std/console2.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
