448b4bedac322f901495fdd7d7bea5acd9cfb2a8
gotnoshoeson <miles_patterson@icloud.com>
2024-07-27 18:01:24 -0500

    Start writing tests

--- packages/foundry/test/Challenge.t.sol
+++ packages/foundry/test/Challenge.t.sol
@@ -2,8 +2,30 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "../contracts/Challenge.sol";
+import "../contracts/SignatureVoting.sol";
 
 contract ChallengeTest is Test {
-    // Put tests here
+    // Declare variables
+    SignatureVoting public signatureVoting;
+
+    // Create users
+    address public userOne = address(0x123);
+    address public userTwo = address(0x456);
+    address public userThree = address(0x782);
+
+
+    // Deploy contract, no constructor
+    function setUp() public {
+        signatureVoting = new SignatureVoting();
+        signatureVoting.createProposal(); // need bytes32 data here
+        
+    }
+
+    // Create proposals
+
+    // Test that voters voted
+
+    // Test for duplicate votes for single proposal
+
+
 }
